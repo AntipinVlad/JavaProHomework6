efc8dcddc7551f2399d7d70c10e30ad2c9353f46
AntipinVlad <antipinmail1@gmail.com>
2024-03-31 12:06:07 +0300

    add list node functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3,
+                new ListNode(4, new ListNode(5)))));
+
+        ListNode i = head.reverseList(head);
+        while(i != null) {
+            System.out.print(i.val + " ");
+            i = i.next;
+        }
     }
 }
